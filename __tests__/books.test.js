5b0fa8a19fe6c14b39f906fb9c4c569087852170
austin.gardner_bespin <austin.gardner.3@teambespin.us>
2022-12-15 11:00:53 -0600

    linter fixes

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,38 +1,37 @@
-import { ApolloServer } from "@apollo/server";
-import { typeDefs } from "../src/schema.js";
-import { resolvers } from "../src/resolvers.js";
-import { authors, books, categories } from "../src/schema.js";
+import { ApolloServer } from '@apollo/server';
+import { typeDefs } from '../src/schema.js';
+import { resolvers } from '../src/resolvers.js';
 
-describe("Books Tests", () => {
-	let testServer;
+describe('Books Tests', () => {
+  let testServer;
 
-	beforeEach(() => {
-		const startServer = () => new ApolloServer({ typeDefs, resolvers });
-		testServer = startServer();
-	});
+  beforeEach(() => {
+    const startServer = () => new ApolloServer({ typeDefs, resolvers });
+    testServer = startServer();
+  });
 
-	describe("Testing getBooks", () => {
-		const query = `
+  describe('Testing getBooks', () => {
+    const query = `
     query GetBooks {
         getBooks{
           id
           title
           coverImage
           description
-        }
+		}
       }
     `;
 
-		it("Should return all books", async () => {
-			const response = await testServer.executeOperation({ query });
-			const { getBooks } = await response.body.singleResult.data;
+    it('Should return all books', async () => {
+      const response = await testServer.executeOperation({ query });
+      const { getBooks } = await response.body.singleResult.data;
 
-			expect(getBooks.length).toBe(4);
-		});
-	});
+      expect(getBooks.length).toBe(4);
+    });
+  });
 
-	describe("Testing addBook", () => {
-		const query = `
+  describe('Testing addBook', () => {
+    const query = `
 mutation AddBook(
   $addBookTitle: String!
   $addBookAuthor: ID!
@@ -55,28 +54,28 @@ mutation AddBook(
 }
 
   `;
-		it("Should add a book", async () => {
-			const response = await testServer.executeOperation({
-				query,
-				variables: {
-					addBookTitle: "vibes",
-					addBookAuthor: "1",
-					addBookCategories: ["1"],
-					addBookCoverImage: "cool picture",
-					addBookDescription: "fun book",
-				},
-			});
-			const { addBook } = await response.body.singleResult.data;
+    it('Should add a book', async () => {
+      const response = await testServer.executeOperation({
+        query,
+        variables: {
+          addBookTitle: 'vibes',
+          addBookAuthor: '1',
+          addBookCategories: ['1'],
+          addBookCoverImage: 'cool picture',
+          addBookDescription: 'fun book'
+        }
+      });
+      const { addBook } = await response.body.singleResult.data;
 
-			expect(addBook.id).toBe("5");
-			expect(addBook.title).toBe("vibes");
-			expect(addBook.coverImage).toBe("cool picture");
-			expect(addBook.description).toBe("fun book");
-		});
-	});
+      expect(addBook.id).toBe('5');
+      expect(addBook.title).toBe('vibes');
+      expect(addBook.coverImage).toBe('cool picture');
+      expect(addBook.description).toBe('fun book');
+    });
+  });
 
-	describe("Testing getBook", () => {
-		const query = `
+  describe('Testing getBook', () => {
+    const query = `
     query GetBook($getBookId: ID!) {
       getBook(id: $getBookId) {
         id
@@ -93,28 +92,28 @@ mutation AddBook(
     }
     `;
 
-		it("Should return one book based on id", async () => {
-			const response = await testServer.executeOperation({
-				query,
-				variables: { getBookId: "1" },
-			});
-			const { getBook } = await response.body.singleResult.data;
+    it('Should return one book based on id', async () => {
+      const response = await testServer.executeOperation({
+        query,
+        variables: { getBookId: '1' }
+      });
+      const { getBook } = await response.body.singleResult.data;
 
-			expect(getBook.id).toBe("1");
-			expect(getBook.title).toBe("Harry Potter and the Chamber of Secrets");
-			expect(getBook.coverImage).toBe(
-				"https://m.media-amazon.com/images/I/51mFoFmu0EL._AC_SY780_.jpg"
-			);
-			expect(getBook.description).toBe(
-				"Harry Potter and the Chamber of Secrets is a 1998 young adult fantasy novel by J.K. Rowling, the second in the Harry Potter series. The story follows Harry’s tumultuous second year at Hogwarts School of Witchcraft and Wizardry, including an encounter with Voldemort, the wizard who killed Harry’s parents. Against this fantastic backdrop, Rowling examines such themes as death, fame, friendship, choice, and prejudice. Upon release, the novel became a worldwide bestseller and won several awards, including Children’s Book of the Year at the British Book Awards and the Nestlé Smarties Book Award; it was subsequently adapted into a 2002 film directed by Chris Columbus."
-			);
-			expect(getBook.author.id).toBe("1");
-			expect(getBook.categories.length).toBe(2);
-		});
-	});
+      expect(getBook.id).toBe('1');
+      expect(getBook.title).toBe('Harry Potter and the Chamber of Secrets');
+      expect(getBook.coverImage).toBe(
+        'https://m.media-amazon.com/images/I/51mFoFmu0EL._AC_SY780_.jpg'
+      );
+      expect(getBook.description).toBe(
+        'Harry Potter and the Chamber of Secrets is a 1998 young adult fantasy novel by J.K. Rowling, the second in the Harry Potter series. The story follows Harry’s tumultuous second year at Hogwarts School of Witchcraft and Wizardry, including an encounter with Voldemort, the wizard who killed Harry’s parents. Against this fantastic backdrop, Rowling examines such themes as death, fame, friendship, choice, and prejudice. Upon release, the novel became a worldwide bestseller and won several awards, including Children’s Book of the Year at the British Book Awards and the Nestlé Smarties Book Award; it was subsequently adapted into a 2002 film directed by Chris Columbus.'
+      );
+      expect(getBook.author.id).toBe('1');
+      expect(getBook.categories.length).toBe(2);
+    });
+  });
 
-	describe("Testing updateBook", () => {
-		const query = `
+  describe('Testing updateBook', () => {
+    const query = `
 		mutation Mutation(
   $updateBookId: ID!
   $updateBookTitle: String!
@@ -144,26 +143,26 @@ mutation AddBook(
   }
 }`;
 
-		it("Should update the values of a book based on ID", async () => {
-			const response = await testServer.executeOperation({
-				query,
-				variables: {
-					updateBookId: "1",
-					updateBookTitle: "Updated tilt",
-					updateBookAuthor: "2",
-					updateBookCategories: ["2"],
-					updateBookCoverImage: "image",
-					updateBookDescription: "book",
-				},
-			});
-			const { updateBook } = response.body.singleResult.data;
+    it('Should update the values of a book based on ID', async () => {
+      const response = await testServer.executeOperation({
+        query,
+        variables: {
+          updateBookId: '1',
+          updateBookTitle: 'Updated tilt',
+          updateBookAuthor: '2',
+          updateBookCategories: ['2'],
+          updateBookCoverImage: 'image',
+          updateBookDescription: 'book'
+        }
+      });
+      const { updateBook } = response.body.singleResult.data;
 
-			expect(updateBook.id).toBe("1");
-			expect(updateBook.title).toBe("Updated tilt");
-			expect(updateBook.author.id).toBe("2");
-			expect(updateBook.categories[0].id).toBe("2");
-			expect(updateBook.coverImage).toBe("image");
-			expect(updateBook.description).toBe("book");
-		});
-	});
+      expect(updateBook.id).toBe('1');
+      expect(updateBook.title).toBe('Updated tilt');
+      expect(updateBook.author.id).toBe('2');
+      expect(updateBook.categories[0].id).toBe('2');
+      expect(updateBook.coverImage).toBe('image');
+      expect(updateBook.description).toBe('book');
+    });
+  });
 });
